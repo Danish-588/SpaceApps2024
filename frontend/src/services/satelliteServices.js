dd937b992cbe1422f8ea7f68ca9492eb5251a5af
Tapia Danish <132780149+Danish-588@users.noreply.github.com>
2024-10-06 00:15:14 +0530

    using fetch instead of axios

--- frontend/src/services/satelliteServices.js
+++ frontend/src/services/satelliteServices.js
@@ -1,10 +1,12 @@
-import axios from 'axios';
-
 export const getSatellitePassDetails = async (lat, lon) => {
-  try {
-    const response = await axios.get(`/api/satellite-pass?lat=${lat}&lon=${lon}`);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
+    try {
+      const response = await fetch(`/api/satellite-pass?lat=${lat}&lon=${lon}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch satellite pass data');
+      }
+      return await response.json();
+    } catch (error) {
+      throw error;
+    }
+  };
+  
